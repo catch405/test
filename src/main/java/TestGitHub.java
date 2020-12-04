5720f5a3c4df1d270f4f0fd5513e33f5766fba0d
catch405 <923270618@qq.com>
2020-12-04 10:55:29 +0800

    01

--- src/main/java/TestGitHub.java
+++ src/main/java/TestGitHub.java
@@ -3,6 +3,7 @@ public class TestGitHub {
 
 
     public static void main(String[] args) {
+        String address = "tiancj88";
         Integer age = 25;
         String name = "cgs";
         System.out.println(name);
